e15851d352fc2f59fdccd82566d553664f5f68b7
tfreitasf <tfreitasf@gmail.com>
2023-09-20 23:24:38 -0300

    feat: Adiciona tratamento de cliques no RecyclerView
    Esta alteração adiciona um tratamento de cliques aos itens exibidos no RecyclerView da atividade principal. Agora, quando um item é clicado, a ação apropriada é tomada com base no ID do item.
    
    - O clique no primeiro item (ID 1) abre a atividade de cálculo de IMC.
    - O clique no segundo item (ID 2) abre a atividade de histórico de IMC.

--- app/src/main/java/br/com/povengenharia/fitlife/MainActivity.kt
+++ app/src/main/java/br/com/povengenharia/fitlife/MainActivity.kt
@@ -1,6 +1,7 @@
 package br.com.povengenharia.fitlife
 
 
+import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
 import android.view.View
@@ -40,24 +41,25 @@ class MainActivity : AppCompatActivity() {
                 color = Color.YELLOW
             )
         )
-        mainItems.add(
-            MainItem(
-                id = 3,
-                drawabledId = R.drawable.historicoimc,
-                textStringId = R.string.tv_bmi_history_title,
-                color = Color.BLUE
-            )
-        )
-        mainItems.add(
-            MainItem(
-                id = 4,
-                drawabledId = R.drawable.historicoimc,
-                textStringId = R.string.tv_bmi_history_title,
-                color = Color.MAGENTA
-            )
-        )
 
-        val adapter = MainAdapter(mainItems)
+
+        val adapter = MainAdapter(mainItems) {id ->
+            when (id) {
+                    1 -> {
+                        val intent = Intent(this@MainActivity, BmiActivity::class.java)
+                        startActivity(intent)
+                    }
+
+                    2 -> {
+                        val intent = Intent(this@MainActivity, BmiHistoryActivity::class.java)
+                        startActivity(intent)
+
+                    }
+                }
+            }
+
+
+
 
         val layoutManager = GridLayoutManager(this, 2)
 
@@ -68,8 +70,15 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    private inner class MainAdapter(private val mainItems: List<MainItem>) :
-        RecyclerView.Adapter<MainViewHolder>() {
+
+
+
+
+
+    private inner class MainAdapter(
+        private val mainItems: List<MainItem>,
+        private val onItemClickListener: (Int) -> Unit,
+    ) : RecyclerView.Adapter<MainAdapter.MainViewHolder>() {
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainViewHolder {
             val view = layoutInflater.inflate(R.layout.main_item, parent, false)
             return MainViewHolder(view)
@@ -89,17 +98,24 @@ class MainActivity : AppCompatActivity() {
 
         }
 
-    }
 
-    private class MainViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        fun bind(item: MainItem) {
-            val title: TextView = itemView.findViewById(R.id.tv_icon_name)
-            title.setText(item.textStringId)
-            val img: ImageView = itemView.findViewById(R.id.iv_icon_image)
-            img.setImageResource(item.drawabledId)
-            val container: CardView = itemView as CardView
-            container.setBackgroundColor(item.color)
+        private inner class MainViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+            fun bind(item: MainItem) {
+                val title: TextView = itemView.findViewById(R.id.tv_icon_name)
+                title.setText(item.textStringId)
+                val img: ImageView = itemView.findViewById(R.id.iv_icon_image)
+                img.setImageResource(item.drawabledId)
+                val container: CardView = itemView as CardView
+                container.setBackgroundColor(item.color)
 
+                container.setOnClickListener {
+                    onItemClickListener.invoke(item.id)
+                }
+
+            }
         }
+
     }
+
+
 }
